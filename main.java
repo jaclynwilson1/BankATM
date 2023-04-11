195d94a5cc295c7160e7769061ae1a5561c0ec18
Jaclyn Wilson <jaclyn@Jaclyns-MacBook-Pro.local>
2023-04-11 17:40:26 -0400

    Set up basic banking input

--- main.java
+++ main.java
@@ -1,8 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
 class main{
     public static void main(String[] args){
-        System.out.println("Hello World!");
-        while(true){
-            System.out.println("Sup");
+        System.out.println("Welcome to JCWBanking!");
+
+        Scanner in = new Scanner(System.in);
+
+        boolean keep_going = true;
+
+        while (keep_going){
+            int choice = in.nextInt();
+            System.out.println("You choose option " + choice);
+
+            if (choice == 0){
+                keep_going = false;
+            }
         }
+
     }
 }
